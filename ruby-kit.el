b886abd9ce7584336faa88bca0940b1af816e7a5
hhuai <8863824@gmail.com>
2011-01-11 15:26:15 +0800

    .

--- ruby-kit.el
+++ ruby-kit.el
@@ -1,7 +1,9 @@
 
 (add-to-list 'load-path (concat dotfiles-dir "/ruby-kit") )
 (add-to-list 'load-path (concat dotfiles-dir "/ruby-kit/rinari") )
-(add-to-list 'load-path (concat dotfiles-dir "/ruby-kit/rinari-rhtml") )
+(add-to-list 'load-path (concat dotfiles-dir "/ruby-kit/emacs-rails") )
+(add-to-list 'load-path (concat dotfiles-dir "/ruby-kit/rhtml") )
+(add-to-list 'load-path (concat dotfiles-dir "/ruby-kit/textmate") )
 (require 'rvm)
 ;;(rvm-use-default)
 (rvm-use "ree" "rails3")
@@ -11,6 +13,12 @@
 (ido-mode t)
 
 (require 'rhtml-mode)
-
 (require 'rinari)
+(require 'rails)
+
+(require 'textmate)
+(textmate-mode)
+
+(add-to-list 'load-path (concat dotfiles-dir "/plugin/git-emacs") )
+(require 'git-emacs)
 
